7908d7df6fe90bc044b8aa3dc72ea06704bf675d
Marcin Tustin <marcin.tustin@gmail.com>
2018-04-07 18:44:14 -0400

    Colorized mandelbrot with animated, aspect ratio normalized rectangle overlay

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-//var x_offset = 300
-//var y_offset = 200
-// Defaults; should be set in setup
-var max_x = 1200
-var max_y = 800
+var max_x
+var max_y
+
+// used for rectangle normalisation, needed for zooming
+var aspectRatio;
 
 var actual_height_range = 2
 var actual_height_offset = -1
@@ -16,6 +16,10 @@ var max_iterations = 25;
 
 var fromColor;
 var toColor;
+var mbBuffer;
+
+var startRect = null;
+var endRect = null;
 
 function pixelToComplex(x, y) {
   imag = (y / max_y) * actual_height_range + actual_height_offset;
@@ -24,22 +28,42 @@ function pixelToComplex(x, y) {
 }
 
 function setup() {
+  pixelDensity(1);
+  background(255)
   colorMode(HSB);
-  max_x = windowWidth
-  max_y = windowHeight
-  fromColor = color(218, 165, 32);
-  toColor = color(20, 100, 255);
+  blendMode(ADD);
+  strokeWeight(0.1)
+  
+  fromColor = color('hsba(160, 100%, 50%, 1)');
+  toColor = color('hsba(20, 100%, 100%, 1)');
+
+  // needed to set up globals
+  windowResized();
+  
   createCanvas(max_x, max_y);
-  // normally draw is called continuously
-  // but this sucks for this case, because (a) our draw is expensive
-  // (b) it does the same thing every time
-  noLoop();
+  stroke(255) // for rectangle
+  noFill() // also for rectangle
 }
 
 function windowResized() {
   max_x = windowWidth
   max_y = windowHeight
   resizeCanvas(max_x, max_y);
+  mbBuffer = createGraphics(max_x, max_y)
+  drawMandelbrot(mbBuffer)
+  aspectRatio = max_x / max_y;
+}
+
+function mousePressed() {
+  startRect = createVector(mouseX, mouseY)
+}
+
+function mouseDragged() {
+  endRect = createVector(mouseX, mouseY)
+}
+
+function mouseReleased() {
+  // TODO: implement zooming
 }
 
 function nonDivergentMandelbrotIteration(c) {
@@ -55,22 +79,33 @@ function nonDivergentMandelbrotIteration(c) {
   return num_iterations;
 }
 
-function drawMandelbrot() {
-  background(51);
-  stroke(150);
-  point(10, 10);
+function drawMandelbrot(g) {
   for (var x = 0; x < max_x; x++) {
     for (var y = 0; y < max_y; y++) {
-      //console.log([x,y])
       var pxAsComplex = pixelToComplex(x, y);
-      //var inMandelbrot = pxAsComplex.abs() < 2; 
       var mandelVal = nonDivergentMandelbrotIteration(pxAsComplex);
-      stroke(lerpColor(fromColor, toColor, mandelVal / max_iterations));
-      point(x,y)
+      var trueColor = lerpColor(fromColor, toColor, mandelVal / max_iterations);
+      g.stroke(trueColor);
+      g.point(x,y)
     }
   }
 }
 
 function draw() {
-  drawMandelbrot()
+  // draw may be called before this is created
+  if(mbBuffer) {
+    image(mbBuffer, 0, 0)
+  }
+  if(startRect && endRect) {
+    var diff = p5.Vector.sub(endRect,startRect);
+    var width = diff.x
+    var height = diff.y
+    // normalise whichever of width or height is less
+    if (width <= height) {
+      width = height * aspectRatio
+    } else {
+      height = width / aspectRatio
+    }
+    rect(startRect.x, startRect.y, width, height)
+  }
 }
